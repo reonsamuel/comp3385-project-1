2c92ddd43f61de38ce4358e601d07ac58d4dcb70
reon <reonsamuel2001@gmail.com>
2024-10-27 19:37:05 -0400

    exercise 1

---
+++ tic-tac-toe.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const squares = document.querySelectorAll("#board div");
+    squares.forEach(square => {
+      square.classList.add("square");
+    });
+  });
+  
